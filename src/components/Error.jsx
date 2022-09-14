865510af863bcbd14b511ce83016d0f6a8ea23a7
covajg <josegregorio.covag@gmail.com>
2022-09-14 07:58:24 +0200

    primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = ({children}) => {
+  return (
+    <div className='bg-red-800 text-white text-center font-bold p-3 uppercase mb-5 rounded'>
+        {children}
+    </div> 
+  )
+}
+
+export default Error
